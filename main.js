337410036dfbbdfa2a642cfeddfc0f73cce083ed
jj.attaq <jj.attaq@tutanota.com>
2022-05-21 21:34:39 +0200

    made 2D array for grid, filled wi divs; only appends 1 row, won't make grid

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 const container = document.querySelector('.container');
-      // fragment = document.createDocumentFragment();
-const createRow = () => {
-  row = [];
-  const fillArray = () => {
-    for (let i = 0; i < 16; i++) {
-      cell = document.createElement('div'),
-      cell.setAttribute('class', 'cell');
-      row.push(cell);
-    }
+const row = [],
+      grid = [],
+      count = 16, // make user input later
+      fragment = document.createDocumentFragment();
+function fillRow() {
+  for(let i = 0; i < count; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    row.push(cell);
   }
-  const arrayLoop = () => {
-    fillArray();
-    row.forEach(element => {
-      container.appendChild(element);
-    });
-  }
-  arrayLoop();
 }
-createRow();
+function fillGrid() {
+  for (let i = 0; i < count; i++) { 
+    grid.push(row);
+  }
+}
+function wowzers() {
+  for (let i = 0; i < grid.length; i++) { 
+    for (let j = 0; j < grid[i].length; j++) { 
+      fragment.appendChild(grid[i][j]);
+    }
+    container.appendChild(fragment);
+    // find way to make a new row under the previous that was created
+  }
+}
+
+fillRow();
+fillGrid();
+
+    // cell = document.createElement('div'),
+    // cell.setAttribute('class', 'cell');
+    // column.push(cell);
+    // row.push(column);
+    // container.appendChild(column);
