bf953089c5eaf2339980551acf7d3ec118beeba4
jj.attaq <jj.attaq@tutanota.com>
2022-05-23 09:04:02 +0200

    fillGrid() fills  container successfully

--- main.js
+++ main.js
@@ -3,33 +3,24 @@ const row = [],
       grid = [],
       count = 16, // make user input later
       fragment = document.createDocumentFragment();
-function fillRow() {
+function fillRowArray() {
   for(let i = 0; i < count; i++) {
     const cell = document.createElement('div');
     cell.setAttribute('class', 'cell');
     row.push(cell);
   }
 }
-function fillGrid() {
+function fillGridArray() {
   for (let i = 0; i < count; i++) { 
     grid.push(row);
   }
 }
-function wowzers() {
+function fillGrid() {
+  fillGridArray();
   for (let i = 0; i < grid.length; i++) { 
+    fillRowArray();
     for (let j = 0; j < grid[i].length; j++) { 
-      fragment.appendChild(grid[i][j]);
+      container.appendChild(grid[i][j]);
     }
-    container.appendChild(fragment);
-    // find way to make a new row under the previous that was created
   }
 }
-
-fillRow();
-fillGrid();
-
-    // cell = document.createElement('div'),
-    // cell.setAttribute('class', 'cell');
-    // column.push(cell);
-    // row.push(column);
-    // container.appendChild(column);
