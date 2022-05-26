425ed0fcd10a91fa410f28aa657200833c3a7a4c
jj.attaq <jj.attaq@tutanota.com>
2022-05-26 13:29:54 +0200

    addColor() created; hovering over a cell colors it black

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const container = document.querySelector('.container');
 const row = [],
       grid = [],
-      count = 16, // make user input later
-      fragment = document.createDocumentFragment();
+      count = 16; // make user input later
 function fillRowArray() {
   for(let i = 0; i < count; i++) {
     const cell = document.createElement('div');
@@ -17,10 +16,20 @@ function fillGridArray() {
 }
 function fillGrid() {
   fillGridArray();
-  for (let i = 0; i < grid.length; i++) { 
+  for (let i = 0; i < grid.length; i++) {
     fillRowArray();
     for (let j = 0; j < grid[i].length; j++) { 
       container.appendChild(grid[i][j]);
     }
   }
 }
+function addColor() {  
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseover', (event) => {
+      event.target.style.backgroundColor = 'black';
+    })
+  })
+}
+fillGrid();
+addColor();
