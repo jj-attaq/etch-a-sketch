4164dafdf5bcf519a4f66c2ce705f17565e1611b
jj.attaq <jj.attaq@tutanota.com>
2022-05-27 12:41:04 +0200

    moved fillGrid(), determineCellSize() and drawOnHover() to else statement in runCountPrompt

--- main.js
+++ main.js
@@ -8,16 +8,7 @@ function createNewGrid() {
   button.addEventListener('click', () => {
     removeOldGrid();
     runCountPrompt();
-    fillGrid();
-    determineCellSize();
-    drawOnHover(); 
   })
-}
-function runCountPrompt() {
-  count = prompt('Please choose how many cells you would like per side in the drawing grid:' );
-  if (count > 100) {
-    return alert('Please choose a number below 100.');
-  }
 }
 function removeOldGrid() {
   while(row.length > 0) {
@@ -30,12 +21,40 @@ function removeOldGrid() {
     container.lastChild.remove();
   }
 }
+function runCountPrompt() {
+  count = prompt('Please choose how many cells you would like per side in the drawing grid:' );
+  if (count > 100) {
+    alert('Please choose a number below 100.');
+  } else {
+    fillGrid();
+    determineCellSize();
+    drawOnHover(); 
+  }
+}
+function fillGrid() {
+  fillGridArray();
+  for (let i = 0; i < grid.length; i++) {
+    fillRowArray();
+    for (let j = 0; j < grid[i].length; j++) { 
+      fragment.appendChild(grid[i][j]);
+    }
+    container.appendChild(fragment);
+  }
+}
 function determineCellSize() {
   const cells = document.querySelectorAll('.cell');
   const cellSize = `${960/count}px`;
   cells.forEach((cell) => {
     cell.style.width = cellSize;
     cell.style.height = cellSize;
+  })
+}
+function drawOnHover() {  
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseover', (event) => {
+      event.target.style.backgroundColor = 'black';
+    })
   })
 }
 function fillRowArray() {
@@ -49,22 +68,4 @@ function fillGridArray() {
   for (let i = 0; i < count; i++) { 
     grid.push(row);
   }
-}
-function fillGrid() {
-  fillGridArray();
-  for (let i = 0; i < grid.length; i++) {
-    fillRowArray();
-    for (let j = 0; j < grid[i].length; j++) { 
-      fragment.appendChild(grid[i][j]);
-    }
-    container.appendChild(fragment);
-  }
-}
-function drawOnHover() {  
-  const cells = document.querySelectorAll('.cell');
-  cells.forEach((cell) => {
-    cell.addEventListener('mouseover', (event) => {
-      event.target.style.backgroundColor = 'black';
-    })
-  })
 }
